7d6189dd69bf42f0eef7ddebba1af24987674cde
bMsithole19980 <bmsithole1998@gmail.com>
2023-10-20 13:01:18 +0200

    Search functionalit

--- Screens/Restaurant.js
+++ Screens/Restaurant.js
@@ -1,19 +1,40 @@
-import { StyleSheet, Text, View, Image, Pressable, Alert } from 'react-native';
-import React, { useEffect, useState } from 'react';
-import NavBar from '../Components/NavBar';
-import Search from '../Components/Search';
-import { FontAwesome, Ionicons } from '@expo/vector-icons';
-import { collection, getDocs } from 'firebase/firestore';
-import { db } from '../Config/Firebase';
-import { FlatList } from 'react-native-gesture-handler';
+import { StyleSheet, Text, View, Image, Pressable, Alert } from "react-native";
+import React, { useEffect, useState } from "react";
+import NavBar from "../Components/NavBar";
+import Search from "../Components/Search";
+import { FontAwesome, Ionicons } from "@expo/vector-icons";
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "../Config/Firebase";
+import { FlatList, ScrollView } from "react-native-gesture-handler";
 
 export default function Restaurant() {
   const [restaurants, setRestaurants] = useState([]);
-  const [searchQuery, setSearchQuery]= useState()
+  const [searchQuery, setSearchQuery] = useState();
+
+  const menuItem = [
+    {
+      id: 1,
+      name: "Burger",
+      image: "mbeme",
+      rating: "5.0",
+    },
+    {
+      id: 1,
+      name: "Burger",
+      image: "mbeme",
+      rating: "5.0",
+    },
+    {
+      id: 1,
+      name: "Burger",
+      image: "mbeme",
+      rating: "5.0",
+    }
+  ];
 
   useEffect(() => {
     const fetchRestaurantData = async () => {
-      const querySnapshot = await getDocs(collection(db, 'restaurants'));
+      const querySnapshot = await getDocs(collection(db, "restaurants"));
       const data = [];
       querySnapshot.forEach((doc) => {
         data.push(doc.data());
@@ -23,23 +44,41 @@ export default function Restaurant() {
     fetchRestaurantData();
   }, []);
 
-  const [favouriteRestaurant, setFavouriteRestaurant]= useState(new Set());
+  const [favouriteRestaurant, setFavouriteRestaurant] = useState(new Set());
 
-  const filterRestaurants=()=>{
+  const filterRestaurants = () => {
     if (searchQuery) {
-        const filterdRestuarants = restaurants.filter((restaurant)=>
-     restaurant.RestaurantName.toLowerCase().includes(searchQuery.toLowerCase())
-    );
-    return filterRestaurants;
+      const filterdRestuarants = restaurants.filter((restaurant) =>
+        restaurant.RestaurantName.toLowerCase().includes(
+          searchQuery.toLowerCase()
+        )
+      );
+      return filterRestaurants;
     }
     restaurants;
-  }
+  };
   return (
     <View style={styles.container}>
       <NavBar />
       <Search
-      value={searchQuery}
-      onChangeText={(text)=> setSearchQuery(text)}/>
+        value={searchQuery}
+        onChangeText={(text) => setSearchQuery(text)}
+      />
+
+      <View style={styles.categories}>
+        <View style={styles.cat}>
+          <Text style={styles.catName}>Categories</Text>
+             <View style={styles.menuItems}>
+            {menuItem.map((menuItem) => (
+              <View style={styles.menu}>
+                <Pressable styles={styles.menuName}>
+                  <Text>Burger</Text>
+                </Pressable>
+              </View>
+            ))}
+          </View>
+        </View>
+      </View>
 
       <View style={styles.restaurant}>
         <View style={styles.res}>
@@ -50,18 +89,25 @@ export default function Restaurant() {
           data={restaurants}
           renderItem={({ item }) => (
             <View style={styles.resContainer}>
-              <Image style={styles.resImage} source={{ uri: item.RestaurantImage }} />
+              <Image
+                style={styles.resImage}
+                source={{ uri: item.RestaurantImage }}
+              />
               <Text style={styles.resName}>{item.RestaurantName}</Text>
               <Text style={styles.delivery}>{item.RestaurantAddress}</Text>
-              <Pressable
-              onPress={()=> toggleFavorite(item.RestaurantName)}>
-              <Ionicons 
-              style={styles.fav} 
-              name="heart" 
-              size={24} 
-              color={favouriteRestaurant.has(item.RestaurantName)? 'red' : 'grey'} />
+              <Pressable onPress={() => toggleFavorite(item.RestaurantName)}>
+                <Ionicons
+                  style={styles.fav}
+                  name="heart"
+                  size={24}
+                  color={
+                    favouriteRestaurant.has(item.RestaurantName)
+                      ? "red"
+                      : "grey"
+                  }
+                />
               </Pressable>
-             
+
               <View style={styles.rating}>
                 <FontAwesome name="star" size={16} color="grey" />
                 <FontAwesome name="star" size={16} color="grey" />
@@ -82,56 +128,76 @@ export default function Restaurant() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#000000',
+    backgroundColor: "#000000",
+  },
+  cat: {},
+  menu: {
+    width: 100,
+    height: 80,
+    backgroundColor: "#fff",
+    left: 10,
+    marginTop: 10,
+    borderRadius: 10,
+  },
+  menuName: {
+    backgroundColor: "#fff",
+  },
+  catName: {
+    color: "#fff",
+    fontWeight: "bold",
+    marginTop: 40,
+  },
+  menuItems:{
+    flexDirection: "row",
+    gap: 20
   },
   heards: {
-    color: '#fff',
+    color: "#fff",
   },
   hw: {
-    color: '#fff',
+    color: "#fff",
   },
   restaurant: {
     top: 40,
   },
   res: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
+    flexDirection: "row",
+    justifyContent: "space-between",
   },
   all: {
-    color: '#fff',
-    fontWeight: 'bold',
-    fontFamily: 'Poppin',
+    color: "#fff",
+    fontWeight: "bold",
+    fontFamily: "Poppin",
     fontSize: 14,
     left: 10,
   },
   viewAll: {
-    color: '#fff',
-    fontWeight: 'bold',
-    fontFamily: 'Poppin',
+    color: "#fff",
+    fontWeight: "bold",
+    fontFamily: "Poppin",
     fontSize: 13,
     right: 10,
   },
   resContainer: {
-    width: '90%',
+    width: "90%",
     height: 140,
-    backgroundColor: '#fff',
+    backgroundColor: "#fff",
     left: 20,
     top: 50,
     borderRadius: 5,
-    marginBottom: 20
+    marginBottom: 20,
   },
   resImage: {
-    width: '100%',
+    width: "100%",
     height: 70,
-   
   },
   resName: {
-    fontWeight: 'bold',
+    fontWeight: "bold",
     fontSize: 13,
   },
   delivery: {
     fontSize: 10,
-    color: 'grey',
+    color: "grey",
   },
   fav: {
     left: 270,
@@ -139,12 +205,12 @@ const styles = StyleSheet.create({
   },
   rating: {
     bottom: 25,
-    flexDirection: 'row',
+    flexDirection: "row",
   },
   rateNo: {
     left: 100,
     bottom: 40,
     fontSize: 10,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
 });
